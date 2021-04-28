b6a73a9b312a5cbafc0a54928ebe8a11e9abf3e3
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-04-27 19:10:02 -0700

    fixing errors internet out

--- src/App.js
+++ src/App.js
@@ -2,20 +2,46 @@ import { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import CreatureList from './CreatureList';
+import Counter from './Counter';
 import creatures from './creatures';
 import CreatureSearch from './CreatureSearch';
 import './App.css';
 
+const creatureTypes = [...new Set(creatureData.map(c => c.type))];
 
 class App extends Component {
+  state = {
+    creatures: creatureData
+  }
 
+  handleSearch = ({ titleFilter, typeFilter, sortField }) => {
+    const nameRegex = new RegExp(titleFilter, 'i');
+
+    const searchedData = creatureData
+      .filter(creature => {
+        return !nameFilter || cat.name.match(nameRegex);
+      })
+      .filter(cat => {
+        return !typeFilter || cat.type === typeFilter;
+      })
+      .sort((a, b) => {
+        if (a[sortField] < b[sortField]) return -1;
+        if (a[sortField] > b[sortField]) return 1;
+        return 0;
+      });
+    this.setState({ creatures: searchedData });
+  }
   render() {
+    const { creatures } = this.state;
+
     return (
       <div className="App">
 
         <Header />
 
-        <CreatureSearch />
+        <Counter />
+
+        <CreatureSearch types={creatureTypes} onSearch={this.handSearch} />
 
         <main>
           <CreatureList creatures={creatures} />
@@ -24,7 +50,7 @@ class App extends Component {
         <Footer />
 
 
-      </div>
+      </div >
     );
   }
 
--- src/CreatureSearch.js
+++ src/CreatureSearch.js
@@ -2,14 +2,52 @@ import { Component } from 'react';
 import './CreatureSearch.css';
 
 export default class creatureSearch extends Component {
+  state = {
+    titleFilter: '',
+    sortField: '',
+    typeFilter: '',
+  }
+
+  handleNameChange = ({ target }) => {
+    this.setState({ titleFilter: target.value });
+  }
+
+  handleNameChange = ({ target }) => {
+    this.setState({ titleFilter: target.value });
+  }
+
+  handleSearchChange = ({ target }) => {
+    this.setState({ sortField: target.value });
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSearch(this.state);
+  }
+
+  componentDidUpdate(prevProp, prevState) {
+    if (prevState !== this.state) {
+      this.props.onSearch(this.state);
+    }
+  }
 
   render() {
-    return (
-      <form className="CreatureSearch">
-        <input />
+    const { titleFilter, sortField, typeFilter } = this.state;
 
-        <select>
-          <option value="">sort...
+    return (
+      <form className="CreatureSearch" onSubmit={this.handleSubmit}>
+
+        <input
+          name="titleFilter"
+          value={titleFilter}
+          onChange={this.handleNameChange}
+        />
+
+        <select
+          name="typeFilter"
+          value={typeFilter}
+          onChange={this.handleSearchChange}
+        >
+          <option value="">All
           </option>
 
           <option value="title">by title
